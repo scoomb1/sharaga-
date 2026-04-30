29b31110a338cf049473a9c6119c3f457a549be2
BigBobRize2BRZ <zarubin.pm07@gmail.com>
2026-04-30 10:18:14 +0500

    pr5

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\MainController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +18,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function (){
+// Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function (){
+//     Route::get('/', [MainController::class, 'index'])->name('admin.index');
+// });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::resource('/categories',CategoryController::class);
     Route::get('/', [MainController::class, 'index'])->name('admin.index');
 });
